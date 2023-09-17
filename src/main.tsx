ba5c409108020d8a252815d9744079138d31aa98
BKrasnov <bkrasnov235@gmail.com>
2023-09-17 15:41:38 +0700

    Update theme MT-1

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.tsx';
 
-ReactDOM.createRoot(document.getElementById('root')!).render(
+import './theme/index.css';
+
+const rootElement: HTMLElement | null = document.getElementById('root');
+if (rootElement === null) {
+  throw new Error('Failed to find root element');
+}
+
+ReactDOM.createRoot(rootElement).render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
